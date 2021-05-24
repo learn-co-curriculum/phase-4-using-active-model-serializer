281bac2659043012675ddbff3fd99fb8b1a50872
ihollander <ianholla@gmail.com>
2021-05-24 15:08:04 -0400

    Fixed code formatting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,17 @@
 class MoviesController < ApplicationController
 
-    def index
-        movies = Movie.all
-        render json: movies
-    end
+  def index
+    movies = Movie.all
+    render json: movies
+  end
 
-    def show
-        movie = Movie.find_by(id: params[:id])
-        if movie
-            render json: movie
-        else
-            render json: { error: "Movie not found" }, status: :not_found
-        end
+  def show
+    movie = Movie.find_by(id: params[:id])
+    if movie
+      render json: movie
+    else
+      render json: { error: "Movie not found" }, status: :not_found
     end
+  end
+
 end
